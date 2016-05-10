1692acf0d4eca03b9169ba9466f41622cdce62bd
Jared Grippe <jared@learnup.com>
2016-05-10 12:09:41 -0700

    WIP

--- client/router.js
+++ client/router.js
@@ -3,15 +3,9 @@ import NotFoundPage  from './pages/NotFoundPage.jsx'
 import HomePage      from './pages/HomePage.jsx'
 import TransfersPage from './pages/TransfersPage.jsx'
 
-const redirectTo = (path, params) => {
-  params = params || {};
-  return () => {
-    RedirectComponent({path: path, params: params});
-  };
-};
-
 export default new Router( router => {
   router.match('/',          HomePage);
   router.match('/transfers', TransfersPage);
+  // router.match('/files',     router.redirectTo('/transfers'));
   router.match('/*path',     NotFoundPage);
 });
