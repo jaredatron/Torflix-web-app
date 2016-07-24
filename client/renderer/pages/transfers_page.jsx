53655e86cd29c89e9ac6d1d1110be52577048361
Jared Grippe <jared@deadlyicon.com>
2016-07-24 14:52:57 -0700

    simplified transfers page components

--- client/renderer/pages/transfers_page.jsx
+++ client/renderer/pages/transfers_page.jsx
@@ -23,58 +23,48 @@ export default class TransfersPage extends Page {
 }
 
 
-class TorrentDownloads extends React.Component {
-  render(){
-    const downloads = this.props.ids.map(id => {
-      return <TorrentDownload key={id} id={id} details={this.props.downloads[id]} />
-    })
-    return <div>{downloads}</div>
-  }
+const TorrentDownloads = ({ids, downloads}) => {
+  const downloadComponents = ids.map(id => {
+    return <TorrentDownload key={id} id={id} details={downloads[id]} />
+  })
+  return <div>{downloadComponents}</div>
 }
 
-class TorrentDownload extends React.Component {
-  render(){
-    const {id, details} = this.props
-    const torrentName = details.torrentName || details.name || torrentId
-    const stateDescription = (
-      details.error ? 'ERROR '+details.error :
-      details.magnetLink ? 'Adding magnet link to Put.io' :
-      details.trackers ? 'Searching for magnet link' :
-      'Searching for trackers'
-    )
-    return <div className="transfers-torrent-download">
-      <div><strong>{torrentName}</strong></div>
-      <div><small>{stateDescription}</small></div>
+const TorrentDownload = ({id, details}) => {
+  const torrentName = details.torrentName || details.name || torrentId
+  const stateDescription = (
+    details.error ? 'ERROR '+details.error :
+    details.magnetLink ? 'Adding magnet link to Put.io' :
+    details.trackers ? 'Searching for magnet link' :
+    'Searching for trackers'
+  )
+  return <div className="transfers-torrent-download">
+    <div><strong>{torrentName}</strong></div>
+    <div><small>{stateDescription}</small></div>
+  </div>
+}
+
+const Transfers = ({loaded, transfers}) => {
+  if (!loaded) return <div>Loading...</div>
+
+  const transferComponents = transfers.map((transfer) => {
+    return <Transfer key={transfer.id} transfer={transfer} />
+  })
+  transferComponents.reverse()
+
+  return <div className="transfers-list">{transferComponents}</div>
+}
+
+const Transfer = ({transfer}) => {
+
+  const name = transfer.file_id ?
+    <Link path={`/files/${transfer.file_id}`}>{transfer.name}</Link> :
+    <div>{transfer.name}</div>
+
+  return <div className="transfers-list-transfer">
+    {name}
+    <div>
+      <small>{transfer.status_message}</small>
     </div>
-  }
-}
-
-class Transfers extends React.Component {
-  render(){
-    if (!this.props.loaded) return <div>Loading...</div>
-
-    const transfers = this.props.transfers.map((transfer) => {
-      return <Transfer key={transfer.id} transfer={transfer} />
-    })
-    transfers.reverse()
-
-    return <div className="transfers-list">{transfers}</div>
-  }
-}
-
-class Transfer extends React.Component {
-  render(){
-    const transfer = this.props.transfer
-
-    const name = transfer.file_id ?
-      <Link path={`/files/${transfer.file_id}`}>{transfer.name}</Link> :
-      <div>{transfer.name}</div>
-
-    return <div className="transfers-list-transfer">
-      {name}
-      <div>
-        <small>{transfer.status_message}</small>
-      </div>
-    </div>
-  }
+  </div>
 }
